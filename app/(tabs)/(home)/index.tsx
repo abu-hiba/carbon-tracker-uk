9891843899beb92760f313aa1e5a8a05fb3c0b76
Tom Bolton <tbolton96@icloud.com>
2024-06-26 21:05:14 +0100

    Finish basic homepage showing current carbon intensity data

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,26 +1,36 @@
+import { ThemedScrollView } from '@/components/ThemedScrollView';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import { CarbonIntensityData, carbonIntensityAPI } from '@/services/carbonIntensityAPI';
 //import { Link } from 'expo-router';
-import { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, RefreshControl } from 'react-native';
 
 export default function HomeScreen() {
     const [carbonIntensityData, setCarbonIntensityData] = useState<CarbonIntensityData>();
     const [error, setError] = useState<Error>();
     const [loading, setLoading] = useState(true);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const getCarbonIntensity = useCallback(async () => {
+      const { data, error } = await carbonIntensityAPI.get('/intensity');
+      if (error) {
+        setError(error);
+      } else {
+        setCarbonIntensityData(data);
+      }
+    }, []);
+
+    const refreshCarbonIntensity = async () => {
+      setRefreshing(true);
+      await getCarbonIntensity();
+      setRefreshing(false);
+    };
 
     useEffect(() => {
-      const getCarbonIntensity = async () => {
-        const { data, error } = await carbonIntensityAPI.get('/intensity');
-        if (error) {
-          setError(error);
-        } else {
-          setCarbonIntensityData(data);
-        }
+      (async () => {
+        await getCarbonIntensity();
         setLoading(false);
-      };
-      getCarbonIntensity();
+      })();
     }, []);
 
     const displayIntensity = (intensity: number | null): string => {
@@ -31,17 +41,26 @@ export default function HomeScreen() {
     };
 
     return (
-        <ThemedView style={styles.container}>
+        <ThemedScrollView
+          style={styles.scrollContainer}
+          contentContainerStyle={styles.scrollContentContainer}
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={refreshCarbonIntensity} />
+          }>
             {loading && <ThemedText>Loading...</ThemedText>}
 
             {error && <ThemedText>Error: {error.message}</ThemedText>}
 
             {carbonIntensityData && (
-                <ThemedView>
-                    <ThemedText>Carbon Intensity</ThemedText>
-                    <ThemedText>Actual: {displayIntensity(carbonIntensityData.data[0].intensity.actual)}</ThemedText>
-                    <ThemedText>Forecast: {displayIntensity(carbonIntensityData.data[0].intensity.forecast)}</ThemedText>
-                </ThemedView>
+              <>
+                <ThemedText>Carbon Intensity</ThemedText>
+                <ThemedText>From: {carbonIntensityData.data[0].from}</ThemedText>
+                <ThemedText>To: {carbonIntensityData.data[0].to}</ThemedText>
+                <ThemedText>Actual: {displayIntensity(carbonIntensityData.data[0].intensity.actual)}</ThemedText>
+                <ThemedText>Forecast: {displayIntensity(carbonIntensityData.data[0].intensity.forecast)}</ThemedText>
+                <ThemedText>Index: {carbonIntensityData.data[0].intensity.index}</ThemedText>
+              </>
             )}
 
             {/*
@@ -57,15 +76,24 @@ export default function HomeScreen() {
                 </Link>
             ))}
             */}
-        </ThemedView>
+    </ThemedScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+    safeArea: {
+      flexGrow: 1,
+  },
+  scrollContentContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+   scrollContainer: {
+    flexGrow: 1,
+  },
+  carbonIntensity: {
+    flexGrow: 1,
+  },
 });
 
