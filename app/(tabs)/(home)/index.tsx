c0ea36f0b67b694059bef00d9e3063a519e8dd1c
Tom Bolton <tbolton96@icloud.com>
2024-06-26 16:48:35 +0100

    Displays current carbon intensity forecast and actual data

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,11 +1,67 @@
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Link } from 'expo-router';
+//import { Link } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 
+type CarbonIntensityData = {
+    data: {
+        from: string;
+        to: string;
+        intensity: {
+            forecast: number;
+            actual: number | null;
+            index: string;
+        };
+    }[];
+};
+
 export default function HomeScreen() {
+    const [carbonIntensityData, setCarbonIntensityData] = useState<CarbonIntensityData>();
+    const [error, setError] = useState<Error>();
+    const [loading, setLoading] = useState(true);
+
+    const getCarbonIntensity = async () => {
+        try {
+            const response = await fetch('https://api.carbonintensity.org.uk/intensity');
+            const data = await response.json();
+            setCarbonIntensityData(data);
+        } catch (error: any) {
+            console.error('Error fetching data: ', error);
+            if (error instanceof Error) setError(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        (async () => {
+            await getCarbonIntensity();
+        })();
+    }, []);
+
+    const displayIntensity = (intensity: number | null): string => {
+        if (intensity === null) {
+            return 'Not available';
+        }
+        return `${intensity} gCO2/kWh`; 
+    };
+
     return (
         <ThemedView style={styles.container}>
+            {loading && <ThemedText>Loading...</ThemedText>}
+
+            {error && <ThemedText>Error: {error.message}</ThemedText>}
+
+            {carbonIntensityData && (
+                <ThemedView>
+                    <ThemedText>Carbon Intensity</ThemedText>
+                    <ThemedText>Actual: {displayIntensity(carbonIntensityData.data[0].intensity.actual)}</ThemedText>
+                    <ThemedText>Forecast: {displayIntensity(carbonIntensityData.data[0].intensity.forecast)}</ThemedText>
+                </ThemedView>
+            )}
+
+            {/*
             <ThemedText>Home</ThemedText>
             {[1000, 1001, 1002, 1003, 1004, 1005].map(id => (
                 <Link
@@ -17,6 +73,7 @@ export default function HomeScreen() {
                     <ThemedText>ThemedView user {id} details</ThemedText>
                 </Link>
             ))}
+            */}
         </ThemedView>
     );
 }
